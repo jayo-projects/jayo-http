0fe16a129d1ae241335971786646e299274093fa
pull-vert <5107035+pull-vert@users.noreply.github.com>
2024-04-25 16:18:21 +0200

    ClientRequestBody

---
+++ core/src/main/kotlin/jayo/http/ClientRequestBodyExtensions.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2024-present, pull-vert and Jayo contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ *
+ * Forked from OkHttp (https://github.com/square/okhttp), original copyright is below
+ *
+ * Copyright (C) 2013 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:JvmName("-ClientRequestBody") // A leading '-' hides this class from Java.
+
+package jayo.http
+
+import jayo.ByteString
+import jayo.http.internal.StandardClientRequestBodies
+import java.io.File
+import java.nio.file.Path
+
+/**
+ * @return a new request body that transmits this string. If [contentType] is null or lacks a charset, it will use
+ * UTF-8.
+ */
+public fun String.toRequestBody(contentType: MediaType? = null): ClientRequestBody =
+    StandardClientRequestBodies.create(this, contentType)
+
+/**
+ * @return a new request body that transmits this ByteString. If [contentType] is null or lacks a charset, it will use
+ * UTF-8.
+ */
+public fun ByteString.toRequestBody(contentType: MediaType? = null): ClientRequestBody =
+    StandardClientRequestBodies.create(this, contentType)
+
+/**
+ * @return a new request body that transmits [byteCount] bytes from this byte array, starting at [offset]. If
+ * [contentType] is null or lacks a charset, it will use UTF-8.
+ * @throws IndexOutOfBoundsException if [offset] or [byteCount] is out of range of this array indices.
+ */
+public fun ByteArray.toRequestBody(
+    contentType: MediaType? = null,
+    offset: Int = 0,
+    byteCount: Int = size,
+): ClientRequestBody = StandardClientRequestBodies.create(this, contentType, offset, byteCount)
+
+/**
+ * @return a new request body that transmits the content of this file. If [contentType] is null or lacks a charset, it
+ * will use UTF-8.
+ */
+public fun File.asRequestBody(contentType: MediaType? = null): ClientRequestBody =
+    StandardClientRequestBodies.create(this, contentType)
+
+/**
+ * @return a new request body that transmits the content of this file path. If [contentType] is null or lacks a charset,
+ * it will use UTF-8.
+ */
+public fun Path.asRequestBody(contentType: MediaType? = null): ClientRequestBody =
+    StandardClientRequestBodies.create(this, contentType)
---
+++ core/src/test/kotlin/jayo/http/internal/ClientRequestBodyTest.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2019 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jayo.http.internal
+
+import jayo.Buffer
+import jayo.buffered
+import jayo.http.asRequestBody
+import jayo.http.toMediaType
+import jayo.sink
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.nio.file.Path
+import kotlin.io.path.createFile
+
+class ClientRequestBodyTest {
+    private lateinit var filePath: Path
+
+    @BeforeEach
+    fun setup(@TempDir tempDir: Path) {
+        filePath = tempDir.resolve("file.txt")
+        filePath.createFile()
+    }
+
+    @Test
+    fun testPath() {
+        assertOnPath { path ->
+            val requestBody = path.asRequestBody()
+
+            assertThat(requestBody.contentByteSize()).isEqualTo(0L)
+        }
+    }
+
+    @Test
+    fun testPathRead() {
+        assertOnPath(content = "Hello") { path ->
+            val requestBody = path.asRequestBody()
+
+            assertThat(requestBody.contentByteSize()).isEqualTo(5L)
+
+            val buffer = Buffer()
+            requestBody.writeTo(buffer)
+            assertThat(buffer.readUtf8()).isEqualTo("Hello")
+        }
+    }
+
+    @Test
+    fun testPathDefaultMediaType() {
+        assertOnPath { path ->
+            val requestBody = path.asRequestBody()
+
+            assertThat(requestBody.contentType()).isNull()
+        }
+    }
+
+    @Test
+    fun testPathMediaType() {
+        assertOnPath { path ->
+            val contentType = "text/plain".toMediaType()
+
+            val requestBody = path.asRequestBody(contentType)
+
+            assertThat(requestBody.contentType()).isEqualTo(contentType)
+        }
+    }
+
+    private inline fun <T> assertOnPath(
+        content: String? = null,
+        fn: (Path) -> T,
+    ): T {
+        filePath.sink().buffered().use { sink ->
+            if (content != null) {
+                sink.writeUtf8(content)
+            }
+        }
+
+        return fn(filePath)
+    }
+}
