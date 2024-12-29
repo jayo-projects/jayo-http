ee7316f7f3e98adf4a3b74d59cf2e1e4a2f9a337
pull-vert <5107035+pull-vert@users.noreply.github.com>
2024-12-29 22:04:33 +0100

    Dns

---
+++ core/src/main/java/jayo/http/Dns.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024-present, pull-vert and Jayo contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ *
+ * Forked from OkHttp (https://github.com/square/okhttp), original copyright is below
+ *
+ * Copyright (C) 2013 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jayo.http;
+
+import jayo.http.internal.DnsSystem;
+import org.jspecify.annotations.NonNull;
+
+import java.net.InetAddress;
+import java.util.List;
+
+/**
+ * A domain name service that resolves IP addresses for host names. Most applications will use the
+ * {@linkplain #SYSTEM system DNS service}, which is the default. Some applications may provide their own implementation
+ * to use a different DNS server, to prefer IPv6 addresses, to prefer IPv4 addresses, or to force a specific known IP
+ * address.
+ *
+ * @implSpec Implementations of this interface must be safe for concurrent use.
+ */
+public interface Dns {
+    /**
+     * @return the IP addresses of {@code hostname}, in the order they will be attempted by Jayo HTTP. If a connection
+     * to an address fails, Jayo HTTP will retry the connection with the next address until either a connection is made,
+     * the set of IP addresses is exhausted, or a limit is exceeded.
+     * @throws jayo.JayoUnknownHostException if {@code hostname} has no known IP address in this Dns.
+     */
+    @NonNull
+    List<InetAddress> lookup(String hostname);
+
+    /**
+     * A DNS that uses {@link InetAddress#getAllByName} to ask the underlying operating system to lookup IP addresses.
+     * Most custom {@link Dns} implementations should delegate to this instance.
+     */
+    @NonNull
+    Dns SYSTEM = new DnsSystem();
+}
---
+++ core/src/main/java/jayo/http/internal/DnsSystem.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2024-present, pull-vert and Jayo contributors.
+ * Use of this source code is governed by the Apache 2.0 license.
+ *
+ * Forked from OkHttp (https://github.com/square/okhttp), original copyright is below
+ *
+ * Copyright (C) 2013 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jayo.http.internal;
+
+import jayo.JayoException;
+import jayo.http.Dns;
+import org.jspecify.annotations.NonNull;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A DNS that uses {@link InetAddress#getAllByName} to ask the underlying operating system to lookup IP addresses.
+ */
+public final class DnsSystem implements Dns {
+    @Override
+    public @NonNull List<InetAddress> lookup(final String hostname) {
+        Objects.requireNonNull(hostname);
+
+        try {
+            try {
+                return Arrays.stream(InetAddress.getAllByName(hostname))
+                        .toList();
+            } catch (NullPointerException e) {
+                final var uhe = new UnknownHostException("Broken system behaviour for dns lookup of " + hostname);
+                uhe.initCause(e);
+                throw uhe;
+            }
+        } catch (UnknownHostException e) {
+            throw JayoException.buildJayoException(e);
+        }
+    }
+}
